0cbf87facdd03a0edf6b3edf8d7fc10e9c0b1a53
menandro <menandro.roxas@gmail.com>
2020-03-03 16:39:31 +0900

    fixed dllhook

--- dllhook/vfs.h
+++ dllhook/vfs.h
@@ -15,24 +15,24 @@
 #define LIB_EXT ".lib"
 #endif
 
-#define CUDA_LIB_PATH "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1/lib/x64/"
+#define CUDA_LIB_PATH "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.2/lib/x64/"
 #pragma comment(lib, CUDA_LIB_PATH "cudart.lib")
 
 #define CV_VER_NUM CVAUX_STR(CV_MAJOR_VERSION) CVAUX_STR(CV_MINOR_VERSION) CVAUX_STR(CV_SUBMINOR_VERSION)
 #pragma comment(lib, LIB_PATH "opencv_core" CV_VER_NUM LIB_EXT)
 #pragma comment(lib, LIB_PATH "opencv_highgui" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_videoio" CV_VER_NUM LIB_EXT)
+//#pragma comment(lib, LIB_PATH "opencv_videoio" CV_VER_NUM LIB_EXT)
 #pragma comment(lib, LIB_PATH "opencv_imgproc" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_calib3d" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_xfeatures2d" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_optflow" CV_VER_NUM LIB_EXT)
+//#pragma comment(lib, LIB_PATH "opencv_calib3d" CV_VER_NUM LIB_EXT)
+//#pragma comment(lib, LIB_PATH "opencv_xfeatures2d" CV_VER_NUM LIB_EXT)
+//#pragma comment(lib, LIB_PATH "opencv_optflow" CV_VER_NUM LIB_EXT)
 #pragma comment(lib, LIB_PATH "opencv_imgcodecs" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_features2d" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_tracking" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_flann" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_cudafeatures2d" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, LIB_PATH "opencv_cudaimgproc" CV_VER_NUM LIB_EXT)
-
+//#pragma comment(lib, LIB_PATH "opencv_features2d" CV_VER_NUM LIB_EXT)
+//#pragma comment(lib, LIB_PATH "opencv_tracking" CV_VER_NUM LIB_EXT)
+//#pragma comment(lib, LIB_PATH "opencv_flann" CV_VER_NUM LIB_EXT)
+//#pragma comment(lib, LIB_PATH "opencv_cudafeatures2d" CV_VER_NUM LIB_EXT)
+//#pragma comment(lib, LIB_PATH "opencv_cudaimgproc" CV_VER_NUM LIB_EXT)
+//
 #pragma comment(lib, LIB_PATH "opencv_video" CV_VER_NUM LIB_EXT)
 
 extern "C" __declspec(dllexport) StereoTgv * Create();
