76030dc40ef855469fa9c8014455851da181235a
kyzhao <zhao.kaiyong@gmail.com>
2020-03-06 15:15:57 +0800

    support CMake

--- test_vfs/main.cpp
+++ test_vfs/main.cpp
@@ -1,28 +1,7 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/optflow.hpp>
+
 #include <stereotgv/stereotgv.h>
-
-#if _WIN64
-#define LIB_PATH "D:/dev/lib64/"
-#define CV_LIB_PATH "D:/dev/lib64/"
-#else
-#define LIB_PATH "D:/dev/staticlib32/"
-#endif
-
-#ifdef _DEBUG
-#define LIB_EXT "d.lib"
-#else
-#define LIB_EXT ".lib"
-#endif
-
-#define CUDA_LIB_PATH "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.2/lib/x64/"
-#pragma comment(lib, CUDA_LIB_PATH "cudart.lib")
-
-#define CV_VER_NUM CVAUX_STR(CV_MAJOR_VERSION) CVAUX_STR(CV_MINOR_VERSION) CVAUX_STR(CV_SUBMINOR_VERSION)
-#pragma comment(lib, CV_LIB_PATH "opencv_core" CV_VER_NUM LIB_EXT)
-#pragma comment(lib, CV_LIB_PATH "opencv_highgui" CV_VER_NUM LIB_EXT) // imshow
-#pragma comment(lib, LIB_PATH "opencv_imgproc" CV_VER_NUM LIB_EXT) // Equilize Histrogram
-#pragma comment(lib, LIB_PATH "opencv_imgcodecs" CV_VER_NUM LIB_EXT) // Opening PNG
-#pragma comment(lib, LIB_PATH "opencv_video" CV_VER_NUM LIB_EXT) // Write Optical Flow
 
 void showDepthJet(std::string windowName, cv::Mat image, float maxDepth, bool shouldWait = true) {
 	cv::Mat u_norm, u_gray, u_color;
